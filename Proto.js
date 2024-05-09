7394b70675d09107fb46037d7076db512c90e914
Yahel (Alexis) Martinez <martinezyahel133@gmail.com>
2024-05-09 11:15:38 -0400

    05/09/24

--- Proto.js
+++ Proto.js
@@ -25,114 +25,131 @@ spans.forEach((span, idx) => {
 // console.log(response);
 // const data = await response.json();
 // console.log(data);\
-const cityname = "brooklyn";
+// const cityname = "new york city";
 const zipcode = 11221;
 const countrycode = "US";
-const button = document.querySelectorAll(".submit-button");
 const apikey = "b27226d32b98fe892632afdcc471e314";
 const outputElement = document.getElementById("city-input");
 const output = document.getElementById("out-put");
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}&units=imperial`;
 
-fetch(apiUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const temperature = data.main.temp;
-    const description = data.weather[0].description;
-    const location = data.name;
-    const feels = data.main.feels_like;
-    const windspeed = data.wind.speed;
-    const weather = data.weather[0].description.toLowerCase();
+const button = document.querySelector("#city-button");
+button.addEventListener("click", async function () {
+  const city = outputElement.value;
+  const responsedata = await fetchweatherData(city);
+  displayweatherData(responsedata);
+});
+// don't touch this!!! unless you want to pass in another
+// prarmeter like the zip-code and city in the future!
+const fetchweatherData = async function (city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const responsedata = await response.json();
+  return responsedata;
+};
+//
+// purpose: take weather data (that's already been fetched)
+// and display it
+const displayweatherData = function (data) {
+  const temperature = data.main.temp;
+  const description = data.weather[0].description;
+  const location = data.name;
+  const feels = data.main.feels_like;
+  const windspeed = data.wind.speed;
+  const weather = data.weather[0].description.toLowerCase();
 
-    let imageSource;
-    switch (weather) {
-      case "overcast clouds":
-      case "scattered clouds":
-        imageSource = "./src/1cloud.png";
-        break;
-      case "few clouds":
-        imageSource = "./src/cloud.png";
-        break;
-      case "clear sky":
-        imageSource = "./src/sun.png";
-        break;
-      case "shower rain":
-      case "rain":
-      case "light intensity drizzle":
-      case "drizzle":
-      case "heavy intensity drizzle":
-      case "drizzle rain":
-      case "heavy intensity drizzle rain":
-      case "heavy shower rain and drizzle":
-      case "shower drizzle":
-      case "light rain":
-      case "moderate rain":
-      case "heavy intensity rain":
-      case "very heavy rain":
-      case "extreme rain":
-      case "light intensity shower rain":
-      case "shower rain":
-      case "heavy intensity shower rain":
-      case "ragged shower rain":
-        imageSource = "./src/rain-2.png";
-        break;
-      case "thunderstorm":
-      case "light thunderstorm":
-      case "heavy thunderstorm":
-      case "ragged thunderstorm":
-        imageSource = "./src/storm.png";
-        break;
-      case "freezing rain":
-        imageSource = "./src/snowcloud.png";
-        break;
-      case "thunderstorm with light rain":
-      case "thunderstorm with rain":
-      case "thunderstorm with heavy rain":
-      case "thunderstorm with light drizzle":
-      case "thunderstorm with heavy drizzle":
-        imageSource = "./src/storm-2.png";
-        break;
-      case "snow":
-      case "light snow":
-      case "heavy snow":
-      case "sleet":
-      case "light shower sleet":
-      case "light rain and snow":
-      case "shower sleet":
-      case "light rain and snow":
-      case "rain and snow":
-      case "light shower snow":
-      case "shower snow":
-      case "heavy":
-        imageSource = "./src/snowy.png";
-        break;
-      //atmosphere weather below
-      case "mist":
-        imageSource = "./src/mist.png";
-        break;
-      case "haze":
-        imageSource = "./src/fog.png";
-        break;
-      case "tornado":
-        imageSource = "./src/hurricane.png";
-        break;
-    }
+  let imageSource;
+  switch (weather) {
+    case "overcast clouds":
+    case "scattered clouds":
+      imageSource = "./src/1cloud.png";
+      break;
+    case "few clouds":
+      imageSource = "./src/cloud.png";
+      break;
+    case "clear sky":
+      imageSource = "./src/sun.png";
+      break;
+    case "shower rain":
+    case "rain":
+    case "light intensity drizzle":
+    case "drizzle":
+    case "heavy intensity drizzle":
+    case "drizzle rain":
+    case "heavy intensity drizzle rain":
+    case "heavy shower rain and drizzle":
+    case "shower drizzle":
+    case "light rain":
+    case "moderate rain":
+    case "heavy intensity rain":
+    case "very heavy rain":
+    case "extreme rain":
+    case "light intensity shower rain":
+    case "shower rain":
+    case "heavy intensity shower rain":
+    case "ragged shower rain":
+      imageSource = "./src/rain-2.png";
+      break;
+    case "thunderstorm":
+    case "light thunderstorm":
+    case "heavy thunderstorm":
+    case "ragged thunderstorm":
+      imageSource = "./src/storm.png";
+      break;
+    case "freezing rain":
+      imageSource = "./src/snowcloud.png";
+      break;
+    case "thunderstorm with light rain":
+    case "thunderstorm with rain":
+    case "thunderstorm with heavy rain":
+    case "thunderstorm with light drizzle":
+    case "thunderstorm with heavy drizzle":
+      imageSource = "./src/storm-2.png";
+      break;
+    case "snow":
+    case "light snow":
+    case "heavy snow":
+    case "sleet":
+    case "light shower sleet":
+    case "light rain and snow":
+    case "shower sleet":
+    case "light rain and snow":
+    case "rain and snow":
+    case "light shower snow":
+    case "shower snow":
+    case "heavy":
+      imageSource = "./src/snowy.png";
+      break;
+    //atmosphere weather below
+    case "mist":
+      imageSource = "./src/mist.png";
+      break;
+    case "haze":
+      imageSource = "./src/fog.png";
+      break;
+    case "tornado":
+      imageSource = "./src/hurricane.png";
+      break;
+    case "dust":
+      imageSource = "./src/dust-2.png";
+      break;
+    case "sand/dust whirls":
+      imageSource = "./src/dust-2.png";
+      break;
+    case "volcanic ash":
+      imageSource = "./src/volcanic.png.png";
+      break;
+  }
 
-    const htmlContent = `
-      <p>Temperature in ${location}: ${temperature}°F</p>
-      <p>Weather: ${description}</p>
-      <p>Feels Like: ${feels}</p>
-      <p>Wind-Speed: ${windspeed} mph</p>
-      ${imageSource ? `<img class="constant" src="${imageSource}">` : ""}
-    `;
+  const htmlContent = `
+  <p>Temperature in ${location}: ${temperature}°F</p>
+  <p>Weather: ${description}</p>
+  <p>Feels Like: ${feels}</p>
+  <p>Wind-Speed: ${windspeed} mph</p>
+  ${imageSource ? `<img class="constant" src="${imageSource}">` : ""}
+`;
 
-    output.innerHTML = htmlContent;
-  })
-  .catch((error) => {
-    console.log("Error:", error);
-  });
+  output.innerHTML = htmlContent;
+};
